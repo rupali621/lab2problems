b415fb32eef3750ea6fe547cc4b037d1100a3f4a
roop <rupali44822@gmail.com>
2023-04-12 11:50:45 -0400

    v1

---
+++ problem4lab2.py
@@ -0,0 +1,6 @@
+# Declare and initialize a list with the name “myNumbers”
+myNumbers = [1,2,3,4,5,6,7,8,9,10]
+print("list ofnumbers\n",myNumbers)
+# using the concept of loops
+greater_than_12 = [num for num in myNumbers if num > 12]
+print("list the number that are larger than 12\n",greater_than_12)
